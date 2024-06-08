039f372afdbd93a541d819caa2a10aa01f5695f4
Kavita <kavita.itinfo@gmail.com>
2024-06-08 13:14:09 +0530

    worked on css

--- views/js/navcss.js
+++ views/js/navcss.js
@@ -5,11 +5,14 @@ const closeBtn = document.querySelector(".closeBtn");
 hamburger.addEventListener("click", () => {
     sideNav.style.width = "250px";
     content.style.transition = "margin-left 0.5s";
-    content.style.marginLeft = '13%';
+    content.style.marginLeft = '16%';
+    content.style.width = '84%';
 });
 
 closeBtn.addEventListener("click", () => {
     sideNav.style.width = "0";
     content.style.transition = "margin-left 0.5s";
     content.style.marginLeft = '0%';
+    content.style.width = '100%';
+
 });
