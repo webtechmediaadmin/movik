6c9702ecdf14ab2f4b965c7951e94c406382504a
abhijeetsenguptaa <senguptabhijeet@gmailcom>
2024-06-08 15:09:50 +0530

    manager delete functionality updated

---
+++ services/manager/DeleteManager.service.js
@@ -0,0 +1,30 @@
+const ManagerModel = require("../../models/manager.model");
+
+async function DeleteManagerService(id) {
+    try {
+        const deleteManagerDetails = await ManagerModel.findOne({ where: { id: id } });
+
+        if (!deleteManagerDetails) {
+            return {
+                status: true,
+                message: "Manager not found"
+            }
+        }
+
+        await deleteManagerDetails.destroy();
+
+        return {
+            status: true,
+            message: "Manager deleted successfully"
+        }
+    } catch (error) {
+        console.error("Error in editing Managers", error.message);
+        return {
+            status: false,
+            message: "An error occurred during editing Managers. Please try again later."
+        };
+    }
+}
+
+
+module.exports = DeleteManagerService;
